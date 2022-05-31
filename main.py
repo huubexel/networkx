8142952d9850a8865206ffd04a49538ed04523c1
huubexel <huubexel@hotmail.com>
2022-05-31 13:19:04 +0200

    Rearranged the code and introduced the tweet_library

--- main.py
+++ main.py
@@ -1,31 +1,68 @@
 import pandas as pd
 import networkx as nx
 from matplotlib import pyplot, patches
+# TODO: post more and clearer comments and add more text to the readme
 
-# Specify where the file is
-filepath = './csgo2000.csv'
 
-# Read in the csv file with pandas
-df = pd.read_csv(
-    filepath,
-    delimiter=',',
-    skiprows=1,                                 # skips the first line of the file
-    encoding='unicode_escape',                  # prevents errors on unknown symbols
-    usecols=['Vertex 1', 'Vertex 2', 'Tweet'],  # only import the columns we use
+def prepare_data():
+    # Specify where the file is
+    filepath = './csgo2000.csv'
+
+    # Read in the csv file with pandas
+    df = pd.read_csv(
+        filepath,
+        delimiter=',',
+        skiprows=1,  # skips the first line of the file
+        encoding='unicode_escape',  # prevents errors on unknown symbols
+        usecols=['Vertex 1', 'Vertex 2', 'Tweet'],  # only import the columns we use
     )
 
-df.columns = ['source_user', 'mentioned_user', 'tweet_text']
+    df.columns = ['source_user', 'mentioned_user', 'tweet_text']
 
-# create network Digraph (G), see README why a Digraph
-G = nx.DiGraph()
+    # Make a dictionary with the tweet texts and add the user that said them to the tweet
+    tweet_library = {}
 
-for source_user, mentioned_user in zip(df.source_user, df.mentioned_user):
+    # create network Digraph (G), see README why a Digraph
+    g = nx.DiGraph()
 
-    if source_user != mentioned_user:
-        G.add_edge(source_user, mentioned_user)  # An edge is the connection (line) between two nodes (tweets, etc.)
+    for source_user, mentioned_user, tweet_text in zip(df.source_user, df.mentioned_user, df.tweet_text):
+
+        if source_user != mentioned_user:
+            g.add_edge(source_user, mentioned_user)  # An edge is the connection (line) between two nodes (tweets, etc.)
+
+        # Add to the tweet_text library
+        tweet_library[source_user] = tweet_text
+
+    return g, tweet_library
 
 
-def set_color_and_importance_measure(betw, pos_nodes, graph):
+def calc_centrality_measures(g):
+    # Calculate in degree centrality
+    in_deg_cent = nx.in_degree_centrality(g)
+
+    # Calculate out degree centrality
+    out_deg_cent = nx.out_degree_centrality(g)
+
+    # Calculate betweenness
+    betweenness = nx.betweenness_centrality(g)
+
+    # Calculate closeness
+    closeness = nx.closeness_centrality(g)
+
+    return in_deg_cent, out_deg_cent, betweenness, closeness, g
+
+
+def make_plot(g):
+    # Add the spring layout
+    pos = nx.spring_layout(g)
+
+    # Create the plot where everything will go on to
+    fig, ax = pyplot.subplots(1, figsize=(20, 16))  # The first number in figsize is the width, the second is the length
+    ax.set_title('CSGO network')  # Set a title
+    return pos, ax
+
+
+def set_color_and_importance_measure(betw, pos_nodes, g):
     betweenness_list = []
     degree_list = []
     final_list = []
@@ -37,8 +74,8 @@ def set_color_and_importance_measure(betw, pos_nodes, graph):
     for node in pos_nodes:
 
         # filling the degree list for later
-        if G.degree[node] > 1:
-            degree_list.append(graph.degree[node])
+        if g.degree[node] > 1:
+            degree_list.append(g.degree[node])
         else:
             degree_list.append(0)
 
@@ -119,48 +156,49 @@ def font_sizes(importance_list):
     return final_list
 
 
-# Calculate in degree centrality
-in_deg_cent = nx.in_degree_centrality(G)
+def set_font_size(pos, font_size_list):
+    counter = 0
+    for node, (x, y) in pos.items():
+        pyplot.text(x, y, node, fontsize=font_size_list[counter], ha='center', va='center')
+        counter += 1
 
-# Calculate out degree centrality
-out_deg_cent = nx.out_degree_centrality(G)
 
-# Calculate betweenness
-betweenness = nx.betweenness_centrality(G)
+def create_networkx(g, pos, ax, color_list):
+    nx.draw_networkx_edges(g, pos, alpha=0.1)  # Alpha is how see-through the edges will be
+    nx.draw_networkx_labels(g, pos, font_size=0)
+    nx.draw_networkx_nodes(
+        g,
+        pos,
+        node_color=color_list     # een lijst geven voor elke node een kleur
+    )
 
-# Calculate closeness
-closeness = nx.closeness_centrality(G)
-
-# Add the spring layout
-pos = nx.spring_layout(G)
-
-# Create the plot where everything will go on to
-fig, ax = pyplot.subplots(1, figsize=(20, 16))  # The first number in figsize is the width, the second is the length
-ax.set_title('CSGO network')  # Set a title
-
-# get node color and the color is also an importance measure here
-color_list = set_color_and_importance_measure(betweenness, pos, G)
-node_size_list = nodes_size(color_list)
-font_size_list = font_sizes(node_size_list)
-
-counter = 0
-for node, (x, y) in pos.items():
-    pyplot.text(x, y, node, fontsize=font_size_list[counter], ha='center', va='center')
-    counter += 1
-
-nx.draw_networkx_edges(G, pos, alpha=0.1)  # Alpha is how see-through the edges will be
-nx.draw_networkx_labels(G, pos, font_size=0)
-nx.draw_networkx_nodes(
-    G,
-    pos,
-    node_color=color_list     # een lijst geven voor elke node een kleur
-)
-
-ax.margins(0, 0)
-ax.axis("off")          # No X or Y axis will be shown
-pyplot.savefig("network.png")
-pyplot.show()           # Will show the figure
+    ax.margins(0, 0)
+    ax.axis("off")          # No X or Y axis will be shown
+    pyplot.savefig("network.png")
+    pyplot.show()           # Will show the figure
 
 # TODO: post more and clearer comments and add more text to the readme
 
 
+def part1(g):
+    in_deg_cent, out_deg_cent, betweenness, closeness, graph = calc_centrality_measures(g)
+    pos, ax = make_plot(graph)
+    color_list = set_color_and_importance_measure(betweenness, pos, graph)
+    node_size_list = nodes_size(color_list)
+    font_size_list = font_sizes(node_size_list)
+    set_font_size(pos, font_size_list)
+    create_networkx(graph, pos, ax, color_list)
+
+
+def part2(tw_lib):
+    pass
+
+
+def main():
+    g, tweet_library = prepare_data()
+    part1(g)
+    part2(tweet_library)
+
+
+if __name__ == "__main__":
+    main()
