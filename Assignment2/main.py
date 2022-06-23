8ede9d840b441413f2f0a4c30f1dfa4db20a42bf
huubexel <huubexel@hotmail.com>
2022-06-23 16:45:45 +0200

    Everything is pep8 now, looks fine, and is finished

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,5 +1,5 @@
 import os
-import re
+
 
 def get_data():
     """
@@ -10,7 +10,7 @@ def get_data():
     """
     file_names = os.listdir('./data')
     data = {}
-    while file_names != []:
+    while file_names:
         file_name = file_names.pop()
         file_list = file_name.split('_')
         file_key = "_".join(file_list[2:4])  # This is the first 2 things after cie_ so f.e. LLLL9_S2976765
@@ -56,7 +56,7 @@ def splitpoint(data):
 # Q2. Which interface has better turn-taking? Define a measure
 # and compare both interfaces in a suitable graph
 # A2 average amount of keypresses between turn switch
-def Q1andQ2(data,Q):
+def q1andq2(data, q):
     total_switches_single = 0
     total_switches_double = 0
     total_length_single = 0
@@ -103,16 +103,16 @@ def Q1andQ2(data,Q):
                     elif data_list[i] != "" and turn == 'o':
                         total_switches_double += 1
                         turn = 's'
-    if Q == 1:
+    if q == 1:
         avg_pres_single = total_switches_single/total_length_single
         avg_pres_double = total_switches_double/total_length_double
-        print("Q1:\nInterface 1 swithed on average {0} times per keypress.".format(round(avg_pres_single,4)))
-        print("Interface 2 swithed on average {0} times per keypress.".format(round(avg_pres_double,4)))
-    elif Q == 2:
+        print("Q1:\nInterface 1 switched on average {0} times per keypress.".format(round(avg_pres_single, 4)))
+        print("Interface 2 switched on average {0} times per keypress.".format(round(avg_pres_double, 4)))
+    elif q == 2:
         avg_length_single = total_length_single/total_switches_single
         avg_length_double = total_length_double/total_switches_double
-        print("Q2:\nInterface 1 had an average of {0} words between switches.".format(round(avg_length_single,4)))
-        print("Interface 2 had an average of {0} words between switches.".format(round(avg_length_double,4)))
+        print("Q2:\nInterface 1 had an average of {0} words between switches.".format(round(avg_length_single, 4)))
+        print("Interface 2 had an average of {0} words between switches.".format(round(avg_length_double, 4)))
 
 
 # turns a conversation dict into two list containing all words typed by
@@ -137,11 +137,27 @@ def word_string(file_dict):
     return self_inter1, self_inter2, other_inter1, other_inter2
 
 
+def q3(data):
+    data_counter = 0
+    percentage_interface1 = 0
+    percentage_interface2 = 0
+    for file_key, sort_and_text in data.items():
+        data_counter += 1
+        self_inter1, self_inter2, other_inter1, other_inter2 = word_string(data[file_key])
+        percentage_interface1 += word_checker(self_inter1.split(), other_inter1.split())
+        percentage_interface2 += word_checker(self_inter2.split(), other_inter2.split())
+
+    print("Q3:\nIn Interface 1 they copied each other’s language {0}% out of 100% on average."
+          .format(round((percentage_interface1 / data_counter) * 100, 4)))
+    print("In Interface 2 they copied each other’s language {0}% out of 100% on average."
+          .format(round((percentage_interface2 / data_counter) * 100, 4)))
+
+
 # Q4. Which is the happier interface?
 # Define a measure and compare both interfaces in a suitable graph
 # A4 use wordlist with happy words, and compare the average amount of
 # happy words per word.
-def Q4(data):
+def q4(data):
     with open("./happy_words.txt") as f:
         happy_words = f.read().split("\n")
     inter1_happy_words_total = 0
@@ -160,28 +176,28 @@ def Q4(data):
         for word in inter2_words:
             if word in happy_words:
                 inter2_happy_words_total += 1
-    print("Q4:\nInterface 1 has an average of {0} happy words per word.".format(round(inter1_happy_words_total/inter1_words_total, 4)))
-    print("Interface 2 has an average of {0} happy words per word.".format(round(inter2_happy_words_total/inter2_words_total, 4)))
+    print("Q4:\nInterface 1 has an average of {0} happy words per word."
+          .format(round(inter1_happy_words_total/inter1_words_total, 4)))
+    print("Interface 2 has an average of {0} happy words per word.".
+          format(round(inter2_happy_words_total/inter2_words_total, 4)))
 
 
-# full_word_set_s, split_data_s = create_workable_text(sort_and_text["s"])
-# full_word_set_o, split_data_o = create_workable_text(sort_and_text["o"])
+def word_checker(s, o):
+    used_words_s = set()
+    used_words_o = set()
+    same_words_counter = 0
+    for word_s, word_o in zip(s, o):
+        used_words_s.add(word_s)
+        used_words_o.add(word_o)
+        if word_s in used_words_o:
+            same_words_counter += 1
+        if word_o in used_words_s:
+            same_words_counter += 1
 
-
-def q3(data):
-    data_counter = 0
-    percentage_interface1 = 0
-    percentage_interface2 = 0
-    for file_key, sort_and_text in data.items():
-        data_counter += 1
-        self_inter1, self_inter2, other_inter1, other_inter2 = word_string(data[file_key])
-        percentage_interface1 += word_checker(self_inter1.split(), other_inter1.split())
-        percentage_interface2 += word_checker(self_inter2.split(), other_inter2.split())
-
-    print("Q3:\nIn Interface 1 they copied each other’s language {0}% out of 100% on average."
-          .format(round((percentage_interface1 / data_counter) * 100, 4)))
-    print("In Interface 2 they copied each other’s language {0}% out of 100% on average."
-          .format(round((percentage_interface2 / data_counter) * 100, 4)))
+    try:
+        return same_words_counter / (len(used_words_s) + len(used_words_o))
+    except ZeroDivisionError:
+        return 0.0
 
 
 def q5(data):
@@ -222,60 +238,29 @@ def q5(data):
           .format(same_emotion_inter2, other_emotion_inter2))
 
 
-def word_checker(s, o):
-    used_words_s = set()
-    used_words_o = set()
-    same_words_counter = 0
-    for word_s, word_o in zip(s, o):
-        used_words_s.add(word_s)
-        used_words_o.add(word_o)
-        if word_s in used_words_o:
-            same_words_counter += 1
-        if word_o in used_words_s:
-            same_words_counter += 1
-
-    try:
-        return same_words_counter / (len(used_words_s) + len(used_words_o))
-    except ZeroDivisionError:
-        return 0.0
-
-
-# def emotion_checker(split_data_being_checked, split_data_checked_on, positive_emotions, negative_emotions):
-
-def emotion_checker(split_data_being_checked, split_data_checked_on, positive_emotions, negative_emotions):
+def emotion_checker(self_inter, other_inter, positive_emotions, negative_emotions):
     same_emotion_counter = 0
     other_emotion_counter = 0
     last_emotion = ""
-    word_counter = 0
-    for word in split_data_being_checked:
-        try:
-            if split_data_checked_on[word_counter] in negative_emotions:
-                last_emotion = "negative"
-            elif split_data_checked_on[word_counter] in positive_emotions:
-                last_emotion = "positive"
 
-            if last_emotion == "negative":
-                if word.lower() in negative_emotions:
-                    same_emotion_counter += 1
-                elif word.lower() in positive_emotions:
-                    other_emotion_counter += 1
-            elif last_emotion == "positive":
-                if word.lower() in positive_emotions:
-                    same_emotion_counter += 1
-                elif word.lower() in negative_emotions:
-                    other_emotion_counter += 1
-            word_counter += 1
-        except IndexError:
-            return other_emotion_counter, same_emotion_counter
+    for self_w, other_w in zip(self_inter, other_inter):
+        if self_w in negative_emotions:
+            last_emotion = "negative"
+        elif self_w in positive_emotions:
+            last_emotion = "positive"
+
+        if last_emotion == "negative":
+            if other_w.lower() in negative_emotions:
+                same_emotion_counter += 1
+            elif other_w.lower() in positive_emotions:
+                other_emotion_counter += 1
+        elif last_emotion == "positive":
+            if other_w.lower() in positive_emotions:
+                same_emotion_counter += 1
+            elif other_w.lower() in negative_emotions:
+                other_emotion_counter += 1
 
     return other_emotion_counter, same_emotion_counter
-
-
-def create_workable_text(data):
-    split_data = "".join(data.split("¦"))
-    clean_data = re.sub('/[sd]+', ' ', split_data)
-    return set(clean_data.split()), clean_data.split()
-
 
 
 # Q6: Although the datafiles do not contain explicit information
@@ -288,7 +273,7 @@ def create_workable_text(data):
 # s file. We estimate that this wil be about the amount
 # of correct and incorrect guesses. Since we made these list ourself,
 # so they are probably not complete
-def Q6(data):
+def q6(data):
     with open("./conformation_words.txt") as f:
         correct_words = f.read().split("\n")
     with open("./disconformation_words.txt") as f:
@@ -311,15 +296,17 @@ def Q6(data):
                 inter2_correct_words_total += 1
             if word in false_words:
                 inter2_false_words_total += 1
-    print("Q6:\nInterface 1 has {0} conformation words and {1} disconformation words.".format(inter1_correct_words_total,inter1_false_words_total))
-    print("Interface 2 has {0} conformation words and {1} disconformation words.".format(inter2_correct_words_total,inter2_false_words_total))
+    print("Q6:\nInterface 1 has {0} conformation words and {1} disconformation words."
+          .format(inter1_correct_words_total, inter1_false_words_total))
+    print("Interface 2 has {0} conformation words and {1} disconformation words."
+          .format(inter2_correct_words_total, inter2_false_words_total))
 
 
 # Q7: Can you think of any other interesting measure(s)?
 # Define them and compare both interfaces in a suitable graph.
 # What is the average turn time? Turn meaning the time before
 # the other person reacts or interupts.
-def Q7(data):
+def q7(data):
     turn_times_inter1 = []
     turn_times_inter2 = []
     total_turns_inter1 = 0
@@ -331,7 +318,7 @@ def Q7(data):
         this_turn_time = 0
         turn = None
         for i in range(len(self)):
-            if turn == None:
+            if turn is None:
                 if self[i] == "":
                     turn = 'o'
                 else:
@@ -385,8 +372,8 @@ def Q7(data):
                         turn = 'o'
                 else:
                     this_turn_time += int(time[i])
-    avg_inter1 = round((sum(turn_times_inter1)/total_turns_inter1)/1000,4)
-    avg_inter2 = round((sum(turn_times_inter2)/total_turns_inter2)/1000,4)
+    avg_inter1 = round((sum(turn_times_inter1)/total_turns_inter1)/1000, 4)
+    avg_inter2 = round((sum(turn_times_inter2)/total_turns_inter2)/1000, 4)
     print("Q7:\nThe average amount of turntime for interface 1 is {0} seconds".format(avg_inter1))
     print("The average amount of turntime for interface 2 is {0} seconds.".format(avg_inter2))
 
@@ -394,19 +381,19 @@ def Q7(data):
 def main():
     raw_data = get_data()
     data = splitpoint(raw_data)
-    Q1andQ2(data, 1)
+    q1andq2(data, 1)
     print()
-    Q1andQ2(data, 2)
+    q1andq2(data, 2)
     print()
     q3(data)
     print()
-    Q4(data)
+    q4(data)
     print()
     q5(data)
     print()
-    Q6(data)
+    q6(data)
     print()
-    Q7(data)
+    q7(data)
 
 
 if __name__ == "__main__":
